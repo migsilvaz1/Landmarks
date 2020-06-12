3b43a734859650c39bddb08dbd96c2565673db89
Mike <migsilvaz1@gmail.com>
2020-06-12 12:46:22 +0200

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Miguel Silva Vázquez on 12/06/2020.
+//  Copyright © 2020 SOIPER. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
